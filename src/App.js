0c323a897978104e89954a59e1171f1d801fe98e
Becky <beckyliao1@gmail.com>
2024-02-21 16:05:17 -0800

    Add cat

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
+import cat from './applecat.jpeg'
 
 function App() {
   return (
-    <div> hi there this is beck's site</div>
+    <>
+      <img style={{width: 450}} src={cat} className="Friend" alt="Friend" />
+      <div> hi there this is beck's site</div>
+    </>
     // <div className="App">
     //   <header className="App-header">
     //     <p>
