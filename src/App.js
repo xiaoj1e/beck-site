a405f6912028c2522bce1a9f33b03282edd09ef5
Becky <beckyliao1@gmail.com>
2024-02-29 11:31:46 -0800

    bio snippet

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
     <>
       <img style={{width: 450}} src={cat} className="Friend" alt="Friend" />
       <div> hi there this is beck's site</div>
+      <br/>
+      <div>beck liao is a community organizer, computer programmer, and illustrator.
+      they currently reside in the mission district of san francisco. reach out to <a href = "mailto: beckmliao@gmail.com">beckmliao@gmail.com</a> for any inquiries</div>
     </>
     // <div className="App">
     //   <header className="App-header">
