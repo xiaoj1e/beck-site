8c0f927b1a33c477639a9e67a4a5b71dcfd4f6b8
Becky <beckyliao1@gmail.com>
2024-05-30 09:21:16 -0700

    Fixing text

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ function App() {
   return (
     <>
       <img style={{width: 450}} src={cat} className="Friend" alt="Friend" />
-      <div> hi there this is beck's site</div>
+      {/* <div> hi there this is beck's site</div> */}
       <br/>
-      <div>beck liao (they/she) is a computer programmer, community organizer, and illustrator &#128444;
-      they currently reside in the mission district of san francisco, on ramaytush ohlone land &#127758; reach out to <a href = "mailto: beckmliao@gmail.com">beckmliao@gmail.com</a> for any inquirie s</div>
+      <div>beck liao (they/she) is a computer programmer, community organizer, and illustrator &#128444;</div>
+      <br/>
+      <div>they currently reside in the mission district of san francisco, on ramaytush ohlone land &#127758; reach out to <a href = "mailto: beckmliao@gmail.com">beckmliao@gmail.com</a> for any inquiries</div>
     </>
     // <div className="App">
     //   <header className="App-header">
